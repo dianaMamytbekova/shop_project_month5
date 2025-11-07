794ecea529825d27d7722a29c5ea42020bf042f6
Diana <dianamamytbekova2006@gmail.com>
2025-11-07 12:54:19 +0600

    hw1

---
+++ product/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+
+    path('categories/', views.CategoryListAPIView.as_view()),
+    path('categories/<int:pk>/', views.CategoryDetailAPIView.as_view()),
+
+    
+    path('products/', views.ProductListAPIView.as_view()),
+    path('products/<int:pk>/', views.ProductDetailAPIView.as_view()),
+
+    
+    path('reviews/', views.ReviewListAPIView.as_view()),
+    path('reviews/<int:pk>/', views.ReviewDetailAPIView.as_view()),
+]
